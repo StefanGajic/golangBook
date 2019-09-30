01a8912b9d6e0b8f5a80431ae80885f00f769284
StefanGajic <stefan.gajic@ymail.com>
2019-09-30 14:07:13 +0200

    fixed code, getting latest comic

--- comics/main.go
+++ comics/main.go
@@ -4,9 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 )
 
-type Comics struct {
+type Comic struct {
 	Month      string
 	Num        int
 	Link       string
@@ -20,70 +21,46 @@ type Comics struct {
 	Day        string
 }
 
-var comic = []Comics{
-	{Month: "9", Num: 2208, Link: "", Year: "2019", News: "", Safe_title: "Drone Fishing", Transcript: "", Alt: "Today's consumers who order their drones off the internet don't know the joy of going out in nature and returning with a drone that you caught yourself, whose angry owners you fought off with your own two hands.", Img: "https://imgs.xkcd.com/comics/drone_fishing.png", Title: "Drone Fishing", Day: "27"},
+const URL = "https://xkcd.com/info.0.json"
+
+func getLastComic() (*Comic, error) {
+	comic, err := getComic(URL)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	return comic, nil
 }
 
-var comic2 = []Comics{
-	{Month: "9", Num: 2207, Link: "", Year: "2019", News: "", Safe_title: "Math Work", Transcript: "", Alt: "I could type this into a solver, which MIGHT help, but would also mean I have to get a lot of parentheses right...", Img: "https://imgs.xkcd.com/comics/math_work.png", Title: "Math Work", Day: "25"},
-}
+func getComic(url string) (*Comic, error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
 
-var comic3 = []Comics{
-	{Month: "9", Num: 2206, Link: "https://www.fonts.com/content/learning/fontology/level-3/numbers/oldstyle-figures", Year: "2019", News: "", Safe_title: "Mavis Beacon", Transcript: "", Alt: "There are actually lowercase-like 'oldstyle' forms of normal numbers with more pronounced ascenders and descenders, which is why some numbers like '5' in books sometimes dangle below the line. But the true capital numbers remain the domain of number maven Mavis Beacon.", Img: "https://imgs.xkcd.com/comics/mavis_beacon.png", Title: "Mavis Beacon", Day: "23"},
-}
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
 
-var comic4 = []Comics{
-	{Month: "9", Num: 2205, Link: "", Year: "2019", News: "", Safe_title: "Types of Approximation", Transcript: "", Alt: "It's not my fault I haven't had a chance to measure the curvature of this particular universe.", Img: "https://imgs.xkcd.com/comics/types_of_approximation.png", Title: "Types of Approximation", Day: "20"},
-}
-
-var comic5 = []Comics{
-	{Month: "9", Num: 2204, Link: "", Year: "2019", News: "", Safe_title: "Ksp 2", Transcript: "", Alt: "\"The committee appreciates that your 2020 launch is on track, but the 'human capital/personnel retention' budget includes a lot more unmarked cash payments than usual. What are th--\" \"Public outreach.\"", Img: "https://imgs.xkcd.com/comics/ksp_2.png", Title: "Ksp 2", Day: "18"},
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+	var comic Comic
+	err = json.NewDecoder(resp.Body).Decode(&comic)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	return &comic, nil
 }
 
 func main() {
 
-	var n int
-	fmt.Print("Hello, for the latest comic press 1. For last 5 comics enter numbers 2-5 : ")
-	fmt.Scanf("%d", &n)
-	switch n {
-	case 1:
-		fmt.Println("Latest comic")
-		data, err := json.MarshalIndent(comic, "", "	")
-		if err != nil {
-			log.Fatalf("JSON marshaling failed: %s", err)
-		}
-		fmt.Printf("%s\n", data)
-	case 2:
-		fmt.Println("Second to latest comic")
-		data, err := json.MarshalIndent(comic2, "", "	")
-		if err != nil {
-			log.Fatalf("JSON marshaling failed: %s", err)
-		}
-		fmt.Printf("%s\n", data)
-
-	case 3:
-		fmt.Println("Third to latest comic")
-		data, err := json.MarshalIndent(comic3, "", "	")
-		if err != nil {
-			log.Fatalf("JSON marshaling failed: %s", err)
-		}
-		fmt.Printf("%s\n", data)
-	case 4:
-		fmt.Println("Third to latest comic")
-		data, err := json.MarshalIndent(comic4, "", "	")
-		if err != nil {
-			log.Fatalf("JSON marshaling failed: %s", err)
-		}
-		fmt.Printf("%s\n", data)
-	case 5:
-		fmt.Println("Third to latest comic")
-		data, err := json.MarshalIndent(comic5, "", "	")
-		if err != nil {
-			log.Fatalf("JSON marshaling failed: %s", err)
-		}
-		fmt.Printf("%s\n", data)
-	default:
-		fmt.Println("Please enter numbers 1-5")
+	comic, err := getLastComic()
+	if err != nil {
+		log.Fatal(err)
 	}
-
+	fmt.Println(comic)
 }
